56c6173130e6ac92e1f69a3d52548ba4517ab8e4
Joaquin Gil Chanza <jogich@floridauniversitaria.es>
2016-12-21 11:28:16 +0100

    ximo

---
+++ forumlario2.php
@@ -0,0 +1,30 @@
+<?php
+
+echo "Nombre: ".$_POST['nombre'];
+echo "<br>";
+
+echo "Apellido: ".$_POST['apellido'];
+echo "<br>";
+
+echo "Curso: ".$_POST['curso'];
+echo "<br>";
+
+echo "Ciclo: ".$_POST['ciclo'];
+echo "<br>";
+
+echo "Calificación PROG: ".$_POST['prog'];
+echo "<br>";
+
+echo "Calificación ED: ".$_POST['ed'];
+echo "<br>";
+
+echo "Calificación LLMM: ".$_POST['llmm'];
+echo "<br>";
+
+echo "Calificación BBDD: ".$_POST['bbdd'];
+echo "<br>";
+
+$media=($_POST['bbdd']+$_POST['llmm']+$_POST['ed']+$_POST['prog'])/4;
+echo "La note media es: ".$media;
+
+ ?>
